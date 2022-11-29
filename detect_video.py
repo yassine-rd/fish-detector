6c1bbcb08141c5d7dbc8cee7ceed710e89d40d71
yassine-rd <yassine.rodani@gmail.com>
2022-11-29 15:01:05 +0100

    updating detection scripts

--- detect_video.py
+++ detect_video.py
@@ -3,6 +3,11 @@ import os
 # comment out below line to enable them
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
+import tensorflow as tf
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 import numpy as np
 import cv2
 import time
@@ -14,14 +19,10 @@ import core.utils as utils
 from core.yolov4 import filter_boxes
 from core.functions import *
 
-import tensorflow as tf
 from tensorflow.python.saved_model import tag_constants
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
 
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-if len(physical_devices) > 0:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 flags.DEFINE_string('framework', 'tf', '(tf, tflite')
 flags.DEFINE_string('weights', './checkpoints/yolov4-416', 'path to weights file')
